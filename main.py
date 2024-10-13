268eb3995543cb62c036f8df423b02858fa85555
amirhossein <ahadibfar@gmail.com>
2024-10-12 19:07:30 -0700

    init

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import redis 
 import requests
-
+import json 
 
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/san%20diego?unitGroup=metric&key=YOUR_API_KEY&contentType=json'
-
-req = requests.get(url)
+try:
+    data = response.json()
+except ValueError:
+    print("Response content is not valid JSON")
